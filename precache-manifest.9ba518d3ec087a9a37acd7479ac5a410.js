0909795b10b50594f1082dbb195cc1a3696e8b12
neoribello <46947156+neoribello@users.noreply.github.com>
2020-10-05 18:16:59 -0700

    Updates

---
+++ precache-manifest.9ba518d3ec087a9a37acd7479ac5a410.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "685f9295a185b4f5610e289896d62883",
+    "url": "/shopify-project/index.html"
+  },
+  {
+    "revision": "69a353f23fb6e6a2be3b",
+    "url": "/shopify-project/static/css/2.69507427.chunk.css"
+  },
+  {
+    "revision": "09d7c18fce933cd0313d",
+    "url": "/shopify-project/static/css/main.9f3a9c99.chunk.css"
+  },
+  {
+    "revision": "69a353f23fb6e6a2be3b",
+    "url": "/shopify-project/static/js/2.f0638810.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shopify-project/static/js/2.f0638810.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09d7c18fce933cd0313d",
+    "url": "/shopify-project/static/js/main.3d759d5e.chunk.js"
+  },
+  {
+    "revision": "f5544607e1f17bb91ecb",
+    "url": "/shopify-project/static/js/runtime-main.8bc4e13f.js"
+  },
+  {
+    "revision": "f8f0a6f9ca6e1b4cd13608d8abd853aa",
+    "url": "/shopify-project/static/media/slide-1.f8f0a6f9.jpg"
+  },
+  {
+    "revision": "e6636915a0c621e62d90262e711bb970",
+    "url": "/shopify-project/static/media/slide-2.e6636915.jpg"
+  },
+  {
+    "revision": "074e490e2b3b211a25bc4402cff6a339",
+    "url": "/shopify-project/static/media/slide-3.074e490e.jpg"
+  },
+  {
+    "revision": "cbc588e1c595101ba29aa0bd67b5bf3e",
+    "url": "/shopify-project/static/media/slide-4.cbc588e1.jpg"
+  },
+  {
+    "revision": "a563605e10d23ccfbb275091d6740c1a",
+    "url": "/shopify-project/static/media/slide-5.a563605e.jpg"
+  }
+]);
